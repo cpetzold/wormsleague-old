75195a588c22de17a4fece7b70d6538862b2d393
Conner Petzold <cpetzold@gmail.com>
2020-08-28 22:31:18 -0600

    Use wa-docker

--- scripts/logFromReplay.ts
+++ scripts/logFromReplay.ts
@@ -1,19 +1,28 @@
 import * as path from "https://deno.land/std/path/mod.ts";
 
-import { Application, Router, Status } from "https://deno.land/x/oak/mod.ts";
+import { Application, Status } from "https://deno.land/x/oak/mod.ts";
 
 import { exec } from "https://deno.land/x/exec/mod.ts";
 
 const app = new Application();
 const port: number = 8080;
 
-const router = new Router();
-
-router.get("/", ({ response }) => {
-  response.body = "Ok";
+// Logger
+app.use(async ({ request, response }, next) => {
+  await next();
+  const rt = response.headers.get("X-Response-Time");
+  console.log(`${request.method} ${request.url} - ${rt}`);
 });
 
-router.post("/", async (context) => {
+// Timing
+app.use(async ({ response }, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  response.headers.set("X-Response-Time", `${ms}ms`);
+});
+
+app.use(async (context) => {
   const { request, response } = context;
   const body = request.body({ type: "form-data" });
   const formData = await body.value.read();
@@ -35,17 +44,22 @@ router.post("/", async (context) => {
     path.extname(file.filename),
     ".log",
   );
+
   await Deno.rename(file.filename, replayFilename);
 
-  await exec(
-    `wine wa/WA.exe /q /getlog ${replayFilename}`,
-  );
+  console.log(`Extracting log from ${replayFilename}`);
 
-  response.body = await Deno.readTextFile(logFilename);
+  try {
+    await exec(
+      `bash -c "docker run --rm -i wa wa-getlog < ${replayFilename} > ${logFilename}"`,
+    );
+
+    response.body = await Deno.readTextFile(logFilename);
+  } catch (error) {
+    console.log(error);
+    context.throw(error);
+  }
 });
 
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-console.log("running on port ", port);
+console.log("Running on port ", port);
 await app.listen({ port });
