892b77918a558270952a9879a96f8eb7b3c3a8cc
Conner Petzold <cpetzold@gmail.com>
2020-10-26 22:48:56 -0700

    Update bot

--- bot/index.ts
+++ bot/index.ts
@@ -1,37 +1,9 @@
-import * as Discord from "discord.js";
-
 import discordIrc from "discord-irc";
-
-const client = new Discord.Client();
-
-client.on("ready", () => {
-  console.log("I am ready!");
-});
-
-client.on("message", (message) => {
-  if (message.author.id === client.user.id) {
-    return;
-  }
-
-  const waLinkMatch = /wa:\/\/(\S+)/.exec(message.content);
-
-  if (waLinkMatch) {
-    const [_m, waLink] = waLinkMatch;
-    message.channel.send({
-      embed: {
-        title: `Join wa://${waLink}`,
-        url: `https://wormsleague.com/api/wa/${encodeURIComponent(waLink)}`,
-      },
-    });
-  }
-});
-
-client.login(process.env.BOT_TOKEN);
 
 const GENERAL_CHANNEL_ID = "752615473111236718";
 
 const commonConfig = {
-  nickname: "WormsLeague",
+  nickname: "|",
   discordToken: process.env.BOT_TOKEN,
   webhooks: {
     [GENERAL_CHANNEL_ID]: process.env.BOT_WEBHOOK,
